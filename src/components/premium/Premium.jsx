bf89e8896dc86ffbad671d508bd0dd87fee1f000
Vlad Shevtsov <vlad@shevtsov.io>
2021-12-08 22:33:22 +0300

    adds premuim to localStorage

--- src/components/premium/Premium.jsx
+++ src/components/premium/Premium.jsx
@@ -1,24 +1,35 @@
 import styles from './Premium.module.scss';
-import {useState} from "react";
+import { useEffect, useState } from "react";
+import { observer } from "mobx-react-lite";
+import useLocalStorage from "../../hooks/useLocalStorage";
 
 
 function Premium({store}) {
+    const [premiumStorage, setPremiumStorage] = useLocalStorage(`premium`);
     const [isPremium, setIsPremium] = useState(false);
+
+    useEffect(() => {
+        if (premiumStorage) {
+            store.updatePremium(premiumStorage);
+            setIsPremium(premiumStorage);
+        }
+    }, []);
 
     const handleChangeCheckbox = (evt) => {
         const isChecked = evt.target.checked;
 
         setIsPremium(isChecked);
         store.updatePremium(isChecked);
+        setPremiumStorage(isChecked);
     };
 
     return (
         <section className={styles.premium}>
-            <input className={`${styles.checkbox} visually-hidden`} onChange={handleChangeCheckbox} type="checkbox" id="premiumCheckbox"/>
+            <input className={`${styles.checkbox} visually-hidden`} onChange={handleChangeCheckbox} type="checkbox" id="premiumCheckbox" checked={isPremium} />
             <label className={styles.label} htmlFor="premiumCheckbox">Премиум</label>
             { isPremium && <span><span className="value"> +100</span> к параметрам</span> }
         </section>
     );
 }
 
-export default Premium;
+export default observer(Premium);
