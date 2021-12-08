24fc9fc5b99340445c570c17f7f79b31772d3be6
Vlad Shevtsov <vlad@shevtsov.io>
2021-12-08 22:04:33 +0300

    adds quenching to localStorage

--- src/components/quenching/quenching.jsx
+++ src/components/quenching/quenching.jsx
@@ -1,18 +1,32 @@
 import styles from './quenching.module.scss';
+import useLocalStorage from "../../hooks/useLocalStorage";
+import { getInputValue } from "../../utils/common";
+import { useEffect } from "react";
+import { observer } from "mobx-react-lite";
+
 
 function Quenching({store}) {
+    const [quenchingStorage, setQuenchingStorage] = useLocalStorage(`quenching`);
+    const quenching = store.quenching;
+
+    useEffect(() => {
+        if (quenchingStorage) store.quenchingUpdate(quenchingStorage);
+    }, []);
+
     const handleChangeQuenching = (evt) => {
         const value = Number(evt.target.value);
 
         store.quenchingUpdate(value);
-    }
+        setQuenchingStorage(value);
+    };
+
     return (
         <section className={styles.quenching}>
             <label className={styles.label}>
-                Закалка <input className={styles.field} onChange={handleChangeQuenching} type="number" placeholder="0" min="0" max="10"/> уровень
+                Закалка <input className={styles.field} onChange={handleChangeQuenching} value={getInputValue(quenching)} type="number" placeholder="0" min="0" max="10"/> уровень
             </label>
         </section>
     );
 }
 
-export default Quenching;
+export default observer(Quenching);
