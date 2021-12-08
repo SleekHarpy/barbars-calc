e9d1ac4bc1783610e73abd6dc46e7eb7df78b688
Vlad Shevtsov <vlad@shevtsov.io>
2021-12-08 22:15:39 +0300

    adds combatSkill to localStorage

--- src/components/combatSkill/CombatSkill.jsx
+++ src/components/combatSkill/CombatSkill.jsx
@@ -1,20 +1,32 @@
 import styles from './CombatSkill.module.scss';
+import { observer } from "mobx-react-lite";
+import useLocalStorage from "../../hooks/useLocalStorage";
+import { getInputValue } from "../../utils/common";
+import { useEffect } from "react";
 
 
 function CombatSkill({store}) {
+    const [combatSkillStorage, setCombatSkillStorage] = useLocalStorage(`combatSkill`);
+    const combatSkill = store.combatSkill;
+
+    useEffect(() => {
+        if (combatSkillStorage) store.updateCombatSkill(combatSkillStorage);
+    }, []);
+
     const handleChangeSkill = (evt) => {
         const value = Number(evt.target.value);
 
         store.updateCombatSkill(value);
+        setCombatSkillStorage(value);
     };
 
     return (
         <section className={styles.skill}>
             <label className={styles.label}>
-                Боевой навык <span className={styles.fieldWrap}>+<input className={styles.field} onChange={handleChangeSkill} type="number" placeholder="0" min="0"/></span> к параметрам
+                Боевой навык <span className={styles.fieldWrap}>+<input className={styles.field} onChange={handleChangeSkill} value={getInputValue(combatSkill)} type="number" placeholder="0" min="0"/></span> к параметрам
             </label>
         </section>
     );
 }
 
-export default CombatSkill;
+export default observer(CombatSkill);
