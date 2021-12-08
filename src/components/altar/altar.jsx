89e20daae095a0b9cc0d41d2ab186d2d8d2e3c79
Vlad Shevtsov <vlad@shevtsov.io>
2021-12-08 18:38:29 +0300

    adds altar to localStorage

--- src/components/altar/altar.jsx
+++ src/components/altar/altar.jsx
@@ -1,18 +1,29 @@
 import styles from './altar.module.scss';
 import {observer} from "mobx-react-lite";
+import useLocalStorage from "../../hooks/useLocalStorage";
+import { useEffect } from "react";
+import { getInputValue } from "../../utils/common";
 
 function Altar({store}) {
+    const [altarStorage, setAltarStorage] = useLocalStorage(`altar`, 0);
+    const altar = store.altar;
+
+    useEffect(() => {
+        if (altarStorage) store.updateAltar(altarStorage);
+    }, []);
+
     const handleChangeAltar = (evt) => {
       const value = Number(evt.target.value);
 
       store.updateAltar(value);
-    }
+      setAltarStorage(value);
+    };
+
     return (
         <section className={styles.altar}>
             <label className={styles.label}>
-                Алтарь <span className={styles.fieldWrap}>+<input className={styles.field} onChange={handleChangeAltar} type="number" placeholder="0" min="0"/>%</span>
+                Алтарь <span className={styles.fieldWrap}>+<input className={styles.field} onChange={handleChangeAltar} value={getInputValue(altar)} type="number" placeholder="0" min="0"/>%</span>
             </label>
-
         </section>
     );
 }
