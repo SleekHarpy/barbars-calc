35339e6cd6031704296114bf541d667707153b38
Vlad Shevtsov <vlad@shevtsov.io>
2022-01-11 22:52:10 +0300

    adds text on the help page

--- src/pages/helpScreen/HelpScreen.jsx
+++ src/pages/helpScreen/HelpScreen.jsx
@@ -1,3 +1,4 @@
+import styles from './HelpScreen.module.scss';
 import Container from '../../components/container/Container';
 import Header from '../../components/header/Header';
 
@@ -6,8 +7,11 @@ function HelpScreen() {
     return (
         <Container>
             <Header />
-            <section>
-                <p>Страница помощи</p>
+            <section className={styles.help}>
+                <p>Если возникли вопросы или нашли ошибку в работе калькулятора, можно написать
+                    в <a className={styles.link} href="https://t.me/barbars_chat" target="_blank" rel="noopener noreferrer">Телеграм чате</a> или
+                    на <a className={styles.link} href="https://barbars.ru/forum/subforum/0/267668" target="_blank" rel="noopener noreferrer">форуме</a> клана «ИМПЕРИЯ»
+                </p>
             </section>
         </Container>
     );
